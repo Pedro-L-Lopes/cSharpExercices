8cc7e574fd21dd69fdc240fc33391fe204587444
Pedro Lucas <pedrolucas122737@gmail.com>
2023-08-15 11:38:54 -0300

    Params

---
+++ 10-ModificadorDeParametros/10-ModificadorDeParametros/Calculadora.cs
@@ -0,0 +1,15 @@
+﻿namespace _10_ModificadorDeParametros
+{
+    internal class Calculadora
+    {
+        public static int sum(params int[] numbers)
+        {
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+            return sum;
+        }
+    }
+}
---
+++ 10-ModificadorDeParametros/10-ModificadorDeParametros/Program.cs
@@ -0,0 +1,14 @@
+﻿using _10_ModificadorDeParametros;
+
+namespace Couse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int resultado = Calculadora.sum(1, 2, 3, 4, 5);
+
+            Console.WriteLine(resultado);
+        }
+    }
+}
