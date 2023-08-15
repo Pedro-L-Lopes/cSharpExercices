01189831a3fec48a14e8444886a5454f59e13927
Pedro Lucas <pedrolucas122737@gmail.com>
2023-08-15 10:29:06 -0300

    Array 1

---
+++ 9-Array/9-Array/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            double[] vet = new double[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                vet[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            double sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+               sum += vet[i];
+            }
+
+            double media = sum / n;
+
+            Console.WriteLine("A media de altura é: " + media.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
