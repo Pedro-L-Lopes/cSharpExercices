50b7dc6c7fb421fdf7b9c38e12ab385bb5ba0288
Pedro Lucas <pedrolucas122737@gmail.com>
2023-08-19 20:45:42 -0300

    StringBuilder

---
+++ 17-StringBuilder/17-StringBuilder/Entities/Comment.cs
@@ -0,0 +1,13 @@
+﻿namespace _17_StringBuilder.Entities
+{
+    internal class Comment
+    {
+        public string Text { get; set; }
+        public Comment() { }
+
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+}
---
+++ 17-StringBuilder/17-StringBuilder/Program.cs
@@ -0,0 +1,41 @@
+﻿using _17_StringBuilder.Entities;
+
+namespace Course
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Comment comment1 = new("Have a nice trip");
+            Comment comment2 = new("Wow that's awesome!");
+
+            Post post1 = new(
+                DateTime.Parse("21/03/2018 13:05:44"),
+                "Traveling to New Zealand",
+                "I'm going to visit this buatiful country!",
+                12
+            );
+
+            post1.AddComment(comment1);
+            post1.AddComment(comment2);
+
+
+            Comment comment3 = new("Good night");
+            Comment comment4 = new("May the force be with you");
+
+            Post post2 = new(
+                DateTime.Parse("28/07/2018 23:14:19"),
+                "Good night guys",
+                "See you tomorrow",
+                5
+            );
+
+            post2.AddComment(comment3);
+            post2.AddComment(comment4);
+
+            Console.WriteLine(post1);
+            Console.WriteLine(post2);
+
+        }
+    }
+}
