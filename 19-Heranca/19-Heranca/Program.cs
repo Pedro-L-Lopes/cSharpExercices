5b574d160ce5a9d8c4cba73d0102bbb14ea54525
Pedro Lucas <pedrolucas122737@gmail.com>
2023-08-20 18:50:34 -0300

    heranca

---
+++ 19-Heranca/19-Heranca/Program.cs
@@ -0,0 +1,14 @@
+﻿using _19_Heranca.Entities;
+
+namespace Course
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            BusinessAccount account = new(8010, "Bob Brown", 100.0, 500.0);
+
+            Console.WriteLine(account.Balance);
+        }
+    }
+}
