9dd4e5114f0b87f2bc9a13206660a2cb2affce34
Pedro Lucas <pedrolucas122737@gmail.com>
2023-08-21 08:50:08 -0300

    Upcasting e downcasting

--- 19-Heranca/19-Heranca/Program.cs
+++ 19-Heranca/19-Heranca/Program.cs
@@ -6,9 +6,36 @@ namespace Course
     {
         public static void Main(string[] args)
         {
-            BusinessAccount account = new(8010, "Bob Brown", 100.0, 500.0);
+            Account acc = new(1001, "Crebin", 0);
+            BusinessAccount bacc = new(1002, "Zezin", 0, 500);
 
-            Console.WriteLine(account.Balance);
+            // Upcasting
+
+            Account acc1 = bacc;
+            Account acc2 = new BusinessAccount(1003, "Cleito", 0, 200);
+            Account acc3 = new SavingsAccount(1004, "Mario", 0, 0.01);
+
+            // Downcasting
+
+            BusinessAccount acc4 = (BusinessAccount)acc2;
+            acc4.Loan(100.0);
+
+            //BusinessAccount acc5 = (BusinessAccount)acc3;
+
+            if (acc3 is BusinessAccount)
+            {
+                BusinessAccount acc5 = (BusinessAccount)acc3;
+                acc5.Loan(200);
+                Console.WriteLine("Loan");
+            }
+
+            if (acc3 is SavingsAccount)
+            {
+                SavingsAccount acc5 = (SavingsAccount)acc3;
+                acc5.UpdateBalance();
+                Console.WriteLine("Update");
+            }
+
         }
     }
 }
