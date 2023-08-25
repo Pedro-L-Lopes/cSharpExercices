d9e3858daf17c2cd32b94424edf4d3796b956c4d
Pedro Lucas <pedrolucas122737@gmail.com>
2023-08-25 19:13:10 -0300

    File e Fileinfo

---
+++ 27-FileFileinfo/27-FileFileinfo/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Course
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string sourcePath = @"C:\Nova pasta/file1.txt";
+            string targetPath = @"C:\Nova pasta/file2.txt";
+
+            try
+            {
+                FileInfo fileInfo = new(sourcePath);
+                fileInfo.CopyTo(targetPath);
+
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+
+                Console.WriteLine("Success!");
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+        }
+    }
+}
