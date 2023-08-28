646ac9fb35f0c9ba20b89f0e63f7b0ce1d4eb7ac
Pedro Lucas <pedrolucas122737@gmail.com>
2023-08-28 20:54:35 -0300

    Exercicio interface

--- 29-ExercicioInterface2/29-ExercicioInterface2/Program.cs
+++ 29-ExercicioInterface2/29-ExercicioInterface2/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using _29_ExercicioInterface2.Entities;
+using _29_ExercicioInterface2.Services;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter contract data");
+            Console.Write("Number: ");
+            int contractNumber = int.Parse(Console.ReadLine());
+            Console.Write("Date (dd/MM/yyyy): ");
+            DateTime contractDate = DateTime.Parse(Console.ReadLine());
+            Console.Write("Contract value: ");
+            double contractValue = double.Parse(Console.ReadLine());
+            Console.Write("Enter number of installments: ");
+            int months = int.Parse(Console.ReadLine());
+
+            Contract contract = new(contractNumber, contractDate, contractValue);
+
+            ContractService contractService = new(new PaypalService());
+            contractService.ProcessContract(contract, months);
+
+            Console.WriteLine("Installments: ");
+            foreach (Installment installment in contract.Installments)
+            {
+                Console.WriteLine(installment);
+            }
+        }
+    }
+}
