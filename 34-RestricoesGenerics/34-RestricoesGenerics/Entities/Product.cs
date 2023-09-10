d4b6555bc43ae090a358115ec37de802e1be9a50
Pedro Lucas <pedrolucas122737@gmail.com>
2023-09-10 19:57:02 -0300

    Restricoes generics

---
+++ 34-RestricoesGenerics/34-RestricoesGenerics/Entities/Product.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+
+namespace Course.Entities
+{
+    class Product : IComparable
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (obj is not Product)
+            {
+                throw new ArgumentException("Comparing error: argument is not an Product");
+            }
+            Product other = obj as Product;
+            return Price.CompareTo(other.Price);
+        }
+
+        public override string ToString()
+        {
+            return Name
+                   + ", "
+                   + Price.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ 34-RestricoesGenerics/34-RestricoesGenerics/Program.cs
@@ -0,0 +1,31 @@
+﻿using Course.Entities;
+using Course.Services;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new();
+
+            Console.Write("Enter the number of products: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] vet = Console.ReadLine().Split(",");
+                double price = double.Parse(vet[1], CultureInfo.InvariantCulture);
+                list.Add(new Product(vet[0], price));
+            }
+
+            CalculationService calculationService = new();
+
+            Product p = calculationService.Max(list);
+
+            Console.WriteLine("Most expensive: ");
+            Console.WriteLine(p);
+        }
+    }
+}
