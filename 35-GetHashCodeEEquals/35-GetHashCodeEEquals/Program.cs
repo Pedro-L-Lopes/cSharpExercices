a83b1c1b3984a29118d8664a363298f8252e23ba
Pedro Lucas <pedrolucas122737@gmail.com>
2023-09-13 09:43:52 -0300

    GetHasCode e Equals

---
+++ 35-GetHashCodeEEquals/35-GetHashCodeEEquals/Program.cs
@@ -0,0 +1,18 @@
+﻿using Course.Entities;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client a = new() { Name = "Maria", Email = "maria@gmail.com" };
+            Client b = new() { Name = "Alex", Email = "alex@gmail.com" };
+
+            Console.WriteLine(a.Equals(b));
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+        }
+    }
+}
