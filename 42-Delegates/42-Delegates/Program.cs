d030075ce3892b681b49581b7afc78e1bf147fdd
Pedro Lucas <pedrolucas122737@gmail.com>
2023-09-16 13:48:24 -0300

    Delegates

---
+++ 42-Delegates/42-Delegates/Program.cs
@@ -0,0 +1,21 @@
+﻿using Course.Services;
+
+namespace Course
+{
+
+    delegate double BinaryNumericOperation(double n1, double n2);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 5;
+
+            BinaryNumericOperation op = CalculationService.Max;
+
+            double result = op(a, b);
+            Console.WriteLine(result);
+        }
+    }
+}
