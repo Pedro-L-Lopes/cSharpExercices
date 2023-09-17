ff05ded124fe94b274e3bed0c3a0ad35ae4ed487
Pedro Lucas <pedrolucas122737@gmail.com>
2023-09-16 22:31:55 -0300

    LINQ com Lambda parte 1

--- 45-LINQ_Lambda/45-LINQ_Lambda/Entities/Category.cs
+++ 45-LINQ_Lambda/45-LINQ_Lambda/Entities/Category.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _45_LINQ_Lambda.Entities
+﻿namespace Course.Entities
 {
     internal class Category
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Tier { get; set; }
     }
 }
--- 45-LINQ_Lambda/45-LINQ_Lambda/Entities/Product.cs
+++ 45-LINQ_Lambda/45-LINQ_Lambda/Entities/Product.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Globalization;
 
-namespace _45_LINQ_Lambda.Entities
+namespace Course.Entities
 {
     internal class Product
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public Category Category { get; set; }
+
+        public override string ToString()
+        {
+            return Id
+                + ", "
+                + Name
+                + ", "
+                + Price.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Category.Name
+                + ", "
+                + Category.Tier;    
+        }
     }
 }
