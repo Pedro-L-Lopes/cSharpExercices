859448d673754cad1281fb2204db148fbe1a2e75
Pedro Lucas <pedrolucas122737@gmail.com>
2023-09-16 21:31:11 -0300

    LINQ e FUNC

---
+++ 44-LINQ/44-LINQ/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Especify the data source
+            int[] numbers = { 1, 2, 3, 4, 5 };
+
+            // Define the qury expression
+            var result = numbers.Where(x => x % 2 == 0).Select(x => x * 10);
+
+            // Execute query
+            foreach (int x in result)
+            {
+                Console.WriteLine(x);
+            }
+
+        }
+    }
+}
